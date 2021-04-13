dfad0fa34530724baa8fe5b2d6f6b166f3e45fb8
Kap1tan1 <ilsafilsaf66@gmail.com>
2021-04-13 20:52:21 +0300

    fourth_commit (single activity switch)

--- app/src/main/java/com/example/anime_app/presenter/view/MainActivity.kt
+++ app/src/main/java/com/example/anime_app/presenter/view/MainActivity.kt
@@ -2,46 +2,15 @@ package com.example.anime_app.presenter.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
 import com.example.anime_app.R
-import com.example.anime_app.data.AnimeRepositoryImpl
-import com.example.anime_app.domain.usecase.AnimeTopUseCase
-import com.example.anime_app.presenter.view.recycler.AnimeRecyclerAdapter
-import com.example.anime_app.presenter.viewmodel.MainViewModel
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import okhttp3.Dispatcher
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val adapter = AnimeRecyclerAdapter()
-
-    private val mainViewModel: MainViewModel by lazy {
-        MainViewModel()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        init()
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, MainFragment.newInstance()).commit()
     }
 
-    private fun init() {
-        rv_main.adapter = adapter
-        firstInitList()
-        initSub()
-    }
-
-    private fun initSub() {
-        mainViewModel.list.observe(this, Observer {
-            adapter.submitList(it)
-        })
-    }
-
-    private fun firstInitList() {
-        mainViewModel.updateList()
-    }
 }
